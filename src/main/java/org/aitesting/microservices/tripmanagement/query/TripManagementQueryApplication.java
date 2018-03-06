0e4c07ce89ec35c543d6d9fe882c84c45f0828fa
Erin Yoo <erinyoo@ufl.edu>
2018-03-06 14:33:08 -0500

    Added @EnableRetry feature and added new axon configuration jar

--- src/main/java/org/aitesting/microservices/tripmanagement/query/TripManagementQueryApplication.java
+++ src/main/java/org/aitesting/microservices/tripmanagement/query/TripManagementQueryApplication.java
@@ -2,7 +2,9 @@ package org.aitesting.microservices.tripmanagement.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class TripManagementQueryApplication {
 
