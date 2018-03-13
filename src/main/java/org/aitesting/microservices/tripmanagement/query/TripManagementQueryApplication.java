01c28d7972371cc6d94b7b2f517130a3437ea221
Erin Yoo <erinyoo@ufl.edu>
2018-03-13 16:25:04 -0400

    Uploaded new jar file

--- src/main/java/org/aitesting/microservices/tripmanagement/query/TripManagementQueryApplication.java
+++ src/main/java/org/aitesting/microservices/tripmanagement/query/TripManagementQueryApplication.java
@@ -3,10 +3,12 @@ package org.aitesting.microservices.tripmanagement.query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 
 @EnableRetry
 @SpringBootApplication
 public class TripManagementQueryApplication {
+    @Retryable(value = IllegalArgumentException.class, maxAttempts = 5)
     public static void main(String[] args) {
         SpringApplication.run(TripManagementQueryApplication.class, args);
     }
